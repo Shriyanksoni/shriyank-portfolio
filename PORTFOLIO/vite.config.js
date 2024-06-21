2c6dba3e720753c61658ae80e386340a49e32f4c
Shriyanksoni <piyushsoni024@gmail.com>
2024-06-21 23:40:12 +0530

    changes2

--- PORTFOLIO/vite.config.js
+++ PORTFOLIO/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/portfolio/src/main.jsx",
+  base: "/PORTFOLIO/",
   plugins: [react()],
 })
